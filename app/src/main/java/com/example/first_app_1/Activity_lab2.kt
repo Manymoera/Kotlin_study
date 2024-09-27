9e804da4556b80c7cf8ed1851fc07aeb68384bab
Manymoera <soloninko80@gmail.com>
2024-09-27 14:41:08 +0300

    lab 2 1 - 5

---
+++ app/src/main/java/com/example/first_app_1/Activity_lab2.kt
@@ -0,0 +1,65 @@
+package com.example.first_app_1
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.ToggleButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import java.util.Date
+
+class Activity_lab2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lab2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val BtnTgl : ToggleButton = findViewById(R.id.toggleButton)
+        val BtnHold : Button = findViewById(R.id.buttonHold)
+        val BtnCount : Button = findViewById(R.id.buttonCount)
+        var ClickCount : Int = 0
+        val TxtHold : TextView = findViewById(R.id.textViewHold)
+        val DatePick : DatePicker = findViewById(R.id.datePicker)
+        BtnTgl.setOnClickListener {
+            if(BtnTgl.isChecked) {
+                BtnTgl.setBackgroundColor(Color.parseColor("#D7B2B2"))
+            } else {
+                BtnTgl.setBackgroundColor(Color.parseColor("#DBD0D0"))
+            }
+        }
+        @Suppress("ClickableViewAccessibility")
+        BtnHold.setOnTouchListener{ v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Button press
+                    TxtHold.text = getString(R.string.buttonHold)
+                    true // return true to handle the event
+                }
+                MotionEvent.ACTION_UP -> {
+                    // Button release
+                    TxtHold.text = getString(R.string.buttonHoldR)
+                    true
+                }
+                else -> false
+            }
+        }
+        BtnCount.setOnClickListener {
+            ClickCount++
+            BtnCount.text = ClickCount.toString()
+        }
+        DatePick.setOnDateChangedListener { datePicker, year, monthOfYear, dayOfMonth ->
+            Log.d("Date picked: ", "Day: $dayOfMonth Month: $monthOfYear Year: $year")
+        }
+    }
+}
